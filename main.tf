0a23819f0e0c0434e8f6047d1e661b802a424366
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-30 15:45:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,23 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-1234"
   acl    = "private"
   
+  resource "aws_kms_key" "mykey" {
+    description             = "This key is used to encrypt bucket objects"
+    deletion_window_in_days = 10
+  }
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
     
 }
