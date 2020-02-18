7ad6905412e99e15f5db47982844a5b4060b647d
Narendra Yalavarthi <nyalavarthi@gmail.com>
2020-02-18 12:20:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,20 @@ resource "aws_kms_key" "mykey" {
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-1234"
   acl    = "private"
-  
+  region   = "us-east-1"
+   
   tags = {
     Name        = "My S3 test bucket"
     Environment = "Sandbox"
     Owner       = "Narendra Yala",
     DataType    = "Test files"  
   }
- 
+   
+  logging {
+    target_bucket = "my-tf-test-bucket-1"
+    target_prefix = "s3logs/us-east-1/"
+  }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
